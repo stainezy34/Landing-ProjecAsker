92be72ee512d438daa33111cb29df29a8576bc53
Custom Solutions <74469642+stainezy34@users.noreply.github.com>
2024-11-14 13:13:52 +0000

    Update vite.config.ts
    changed to allow github to function

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Set base to the root of the repository
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
