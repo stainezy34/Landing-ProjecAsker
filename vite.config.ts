187014a0b6dc2e4bc72a9f82f49bb0c56e7a1f8a
Custom Solutions <74469642+stainezy34@users.noreply.github.com>
2024-11-14 13:49:42 +0000

    Update vite.config.ts
    added base '/Landing-ProjecAsker/',

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Set base to the root of the repository
+  base: '/Landing-ProjecAsker/',  // Set base to the root of the repository
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
